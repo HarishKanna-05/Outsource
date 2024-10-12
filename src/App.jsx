2b9e26c9d497f422f952ad4e44e7ec18c569885f
Harish <harishkannaaravindan@gmail.com>
2024-10-12 16:06:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from './components/globals/Navbar'
+import Search from './components/globals/Search'
+
+function App() {
+
+  return (
+    <>
+          <Navbar />
+          <Search />      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/globals/Search.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {Input,Button} from "@nextui-org/react";
+import { IoSearch } from "react-icons/io5";
+
+function Search() {
+  return (
+    <div className='mt-3' >
+        <div className='flex justify-center '>
+            <Input
+            isClearable
+            className='max-w-lg mx-28'
+            endContent={<IoSearch />}
+            placeholder="Search"
+            />
+                    <Button className='font-semibold' variant="flat" radius="full">
+            My Rentals
+        </Button>
+        </div>
+    </div>
+  )
+}
+
+export default Search
